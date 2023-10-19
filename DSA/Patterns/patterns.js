13e2c976fe5ad47d2eb63f1849cc58b2ba2d225d
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-10-18 19:26:05 -0700

    add notes

---
+++ DSA/Patterns/patterns.js
@@ -0,0 +1,28 @@
+// FREQUENCY COUNTER PATTERN
+
+// Write a function called same, which accepts two arrays.
+// The function should return true if every value in the array has it's corresponding value squared in the second array.
+// The frequency of values must be the same.
+
+// same([1,2,3], [4,1,9]) // true
+// same([1,2,3], [1,9]) // false
+// same([1,2,1], [4,4,1]) // false (must be same frequency)
+
+// A naive solution
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if (correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1);
+    }
+    return true;
+}
+
