6125b4c09062c35013a1e59376ba94077a0fbdf0
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-11-06 11:34:48 -0800

    add notes

--- DSA/Patterns/patterns.js
+++ DSA/Patterns/patterns.js
@@ -149,3 +149,55 @@ function countUniqueValues(arr) {
 }
 
 [1, 2, 3, 3, 3]
+
+
+//! SLIDING WINDOW PATTERN
+
+// This pattern involves creating a window which can either be an array or number from one position to another.
+// Depending on a certain condition, the window either increases or closes (and a new window is created).
+// Very useful for keeping track of a subset of data in an array/string etc.
+
+function maxSubarraySum(arr,num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    // create first window
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    // set tempSum to maxSum
+    tempSum = maxSum;
+    // loop through array starting at index num
+    for(let i = num; i < arr.length; i++) {
+        // subtract first number in window and add next number
+        tempSum = tempSum - arr[i - num] + arr[i];
+        // if tempSum is greater than maxSum, set maxSum to tempSum
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+//! DIVIDE AND CONQUER PATTERN
+
+// This pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data.
+// This pattern can tremendously decrease time complexity.
+// Binary search is a great example of this.
+
+function search(array, val) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+
+        if (currentElement < val) {
+            min = middle + 1;
+        } else if (currentElement > val) {
+            max = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+    return -1;
+}
